602b0e58705b02b440a73d03c9cc47ae72b60e6e
zhangpeng.eic <zhangpeng.eic@bytedance.com>
2021-12-10 16:21:07 +0800

    MOD: 模拟excel多个goroutine写入测试

--- excel_test/excel_test.go
+++ excel_test/excel_test.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"strconv"
+	"sync"
+)
 
+var wg sync.WaitGroup
+
+//模拟多个goroutine相关
+func main() {
+	data := make(chan string)
+	go GenData(data)
+	go GenExcelData(data)
+	wg.Add(2)
+	wg.Wait()
+}
+
+func GenData(data chan string) {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		data <- strconv.Itoa(i + 100)
+	}
+}
+
+func GenExcelData(data chan string) {
+	defer wg.Done()
+	file := excelize.NewFile()
+	row := 0
+	for line := range data {
+		// set data
+		row = row + 1
+		file.SetCellStr("Sheet1", ExcelAxis(0, row), line)
+
+	}
+
+	file.DeleteSheet("Sheet1")
+	_ = file.Save()
+}
+
+func ExcelAxis(col, row int) string {
+	var s string
+	for {
+		if col < 26 {
+			return fmt.Sprintf("%c%s%d", col+'A', s, row+1)
+		}
+		s = fmt.Sprintf("%c%s", col%26+'A', s)
+		col = col/26 - 1
+	}
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/go_playground
+
+go 1.16
+
+require github.com/360EntSecGroup-Skylar/excelize v1.4.1
